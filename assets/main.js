3dd3dfe889bfb3688ede85ffcbbd000c3f9aace1
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-10 15:59:11 +0100

    add container and list number

--- assets/main.js
+++ assets/main.js
@@ -12,25 +12,20 @@ for (let i = 1; i <= 100; i++){
     //per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.    
     } if (i % 3 == 0 && i % 5 == 0){
         console.log(i, "FizzBuzz");
-    } 
+    }
+
+    /*
+    BONUS 1:
+    Crea un container nel DOM , aggiungendo un elemento html con il numero o la     stringa corretta da mostrare.
+    Potete usare vari strumenti per farlo:
+    `append()` oppure
+    `.innerHTML`
+    `.insertAdjacentHTML()`
+    */
+    let contElm = document.querySelector('.cont');
+    contElm.innerHTML += (`<li>${i}</li>`);
 }
-    
 
 
 
-
-
-
-
-
-
-
-/*
-Prima di partire a scrivere codice poniamoci qualche domanda:
-Come faccio a sapere se un numero è divisibile per un altro?
-Abbiamo usato un operatore  nei giorni scorsi che possiamo riusare in questi esercizio?
-Consigli del giorno:
-1. Scriviamo sempre prima dei commenti in italiano o inglese per capire cosa vogliamo fare
-2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-3. Facciamo attenzione all'ordine delle condizioni che usiamo */
 
